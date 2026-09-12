6e5271ce28cacb7e2dab5272204c38c4bbdca059
abhayaos <nepalcodinghouse@gmail.com>
2026-09-12 22:46:02 +0545

    Render emoji as native text instead of CDN images

--- src/components/Emoji.jsx
+++ src/components/Emoji.jsx
@@ -1,25 +1,18 @@
-import twemoji from "@twemoji/api";
-
-const CDN_URL = "https://cdn.jsdelivr.net/gh/jdecked/twemoji@15.1.0/assets/svg/";
-
 export default function Emoji({ char, size = "1em", className = "", style }) {
-  const code = twemoji.convert.toCodePoint(char);
-  const src = `${CDN_URL}${code}.svg`;
-
   return (
-    <img
-      src={src}
-      alt={char}
-      draggable={false}
+    <span
+      aria-hidden="true"
+      role="img"
       className={className}
       style={{
-        width: size,
-        height: size,
+        fontSize: size,
+        lineHeight: 1,
         display: "inline-block",
         verticalAlign: "middle",
-        pointerEvents: "none",
         ...style,
       }}
-    />
+    >
+      {char}
+    </span>
   );
 }
